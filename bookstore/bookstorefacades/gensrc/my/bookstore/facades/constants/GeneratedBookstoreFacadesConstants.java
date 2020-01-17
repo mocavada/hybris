a437b5884bddaccbdce29800281f156aedee1a64
mocavada <marc@awesomewebsite.ca>
2020-01-17 14:18:12 -0500

    done with ex5 flexible search query

---
+++ bookstore/bookstorefacades/gensrc/my/bookstore/facades/constants/GeneratedBookstoreFacadesConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Jan 16, 2020 4:29:40 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package my.bookstore.facades.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedBookstoreFacadesConstants
+{
+	public static final String EXTENSIONNAME = "bookstorefacades";
+	
+	protected GeneratedBookstoreFacadesConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ bookstore/bookstoreinitialdata/gensrc/my/bookstore/initialdata/constants/GeneratedBookstoreInitialDataConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Jan 16, 2020 4:29:40 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package my.bookstore.initialdata.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedBookstoreInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "bookstoreinitialdata";
+	
+	protected GeneratedBookstoreInitialDataConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
